9c89a5eb816f01cc32b42c26e4f5ac3f9cccbe0c
Dima Kovyazin <kovyazinaa11@yandex.ru>
2021-10-04 23:43:10 +0300

    delete test rfc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Form from './Form/Form';
 import './App.css'
-// import Tested from './test/Tested'
+
 
 function App() {
   return (
     <div className="App">
       <Form />
-      {/* <Tested /> */}
     </div>
   );
 }
